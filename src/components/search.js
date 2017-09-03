eee281ebc26ecb6abf37c9d70a0c598c0ac5be11
spkelly <spkelly18@gmail.com>
2017-09-03 15:08:21 -0600

    Created Search Component

--- src/components/search.js
+++ src/components/search.js
@@ -2,18 +2,78 @@ import React, { Component } from 'react';
 
 
 class Search extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      inputValue : '',
+      isValid: true,
+    }
+
+    this.validateInput = this.validateInput.bind(this);
+  }
+
+  renderError(){
+    if(!this.state.isValid){
+      return (
+        <div className="input-error"
+             style={{
+               backgroundColor: '#ff8284',
+               height: '40px',
+               color: '#b81b11',
+               border: 'solid #b81b11 1px',
+               paddingLeft: '5px',
+               borderRadius: '6px',
+               marginTop: '20px',
+               paddingTop: '8px'
+             }}
+        >
+        <p>Please Enter a search term</p>
+        </div>
+      )
+    }
+  }
+  changeValue(e){
+    this.setState({inputValue: e.target.value})
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.validateInput();
+    if(this.state.isValid){
+        console.log('Valid');
+    }
+  }
+
+  validateInput(){
+    if(this.state.inputValue.length == 0){
+      this.setState({isValid: false});
+    }
+    else{
+      this.setState({isValid:true})
+    }
+  }
+
   render(){
     return (
       <div className="search-component">
-      <div className="row">
-        <div className="col-lg-5 col-sm-12">
-        <form>
-          <input type="text" className="form-control search-bar" />
-          <button type="submit" className="btn btn-primary form-control submit search-button" > Search </button>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="row">
+            <div className="col-lg-5 col-sm-12">
+              <input type="text" value={this.state.inputValue}onChange={this.changeValue.bind(this)}className="form-control search-bar" />
+            </div>
+            <div className="col-lg-5 col-sm-12">
+              <button type="submit" className="btn btn-primary form-control submit search-button" > Search </button>
+            </div>
+          </div>
         </form>
+        <div className="col-lg-5 col-sm-12">
+        {this.renderError()}
         </div>
         </div>
-      </div>
+
+
 
     )
   }
