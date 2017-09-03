ebc0c0b7fa667723f8702b8ea5a018dd1018a23a
spkelly <spkelly18@gmail.com>
2017-09-03 15:07:03 -0600

    created navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Navigation extends Component{
   render(){
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar nav">
         <a className="navbar-brand">Legitrack</a>
       </nav>
     )
