f06fcf65f1462e58efb17672a52d53e35b51cc2e
spkelly <spkelly18@gmail.com>
2017-09-09 13:30:45 -0600

    created search result components

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navigation from './navbar';
+
+class Results extends Component{
+  render(){
+    return (
+      <div className="search-results">
+        <Navigation />
+      </div>
+    )
+  }
+}
+
+export default Results
