38f0a8e234232cddd93638334a4459a1a0a947a9
spkelly <spkelly18@gmail.com>
2017-09-10 19:33:48 -0600

    Result list now displays test data

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
-
+import ResultListItem from './resultListItem';
+import SearchPanel from './searchPanel';
 
 class ResultList extends Component{
+  constructor(props){
+    super(props);
+
+    this.renderResults = this.renderResults.bind(this);
+  }
+
+  renderResults(){
+    let resultList = _.map(this.props.list, (bill) =>{
+      if(bill.bill_number){
+        return (
+          <ResultListItem key={bill.bill_id} bill={bill} />
+        )
+      }
+    });
+    return resultList;
+  }
+
   render(){
+    console.log('this.props: ', this.props.list)
     return (
-      <div className="result-list">
+      <div className="search-main">
+        <div className="row">
+          <div className="col-md-2 search-col">
+            <SearchPanel/>
+          </div>
+          <div className="col-md-10">
+            <hr className="text-center results-title"></hr>
+            <div className="list-group">
+              {this.renderResults()}
+            </div>
+          </div>
+
+        </div>
+
       </div>
     )
   }
