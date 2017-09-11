d86f4ba3f0cbcb74d23140aa1fad6d3d6ab5d392
spkelly <spkelly18@gmail.com>
2017-09-10 19:35:20 -0600

    modified test data into array before passing it on to result list component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import Navigation from './navbar';
 import Footer from './footer';
+import ResultList from  './ResultList';
+import testData from '../otherstuff/testData';
+import _ from 'lodash';
+
 
 class Results extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {testData: _.toArray(testData.results.searchresult) }
+  }
   render(){
+    console.log(this.state)
     return (
       <div className="search-results">
         <Navigation />
+        <div className="results-main">
+          <ResultList list={this.state.testData}></ResultList>
+        </div>
         <Footer/>
       </div>
     )
