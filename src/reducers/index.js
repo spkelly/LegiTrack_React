8c57f223c7fac8d4bd8ffb5e8aa591963793a235
spkelly <spkelly18@gmail.com>
2017-09-18 10:38:47 -0600

    added bill text reducer to combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import BillsReducer from './reducer_bills';
 import ActiveBill from './reducer_active_bill';
+import getBillText from './reducer_bill_text';
 
 const rootReducer = combineReducers({
     bills: BillsReducer,
-    activeBill: ActiveBill
+    activeBill: ActiveBill,
+    activeBillText: getBillText
 });
 
 export default rootReducer;
