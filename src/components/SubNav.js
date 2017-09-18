e628927e961b2ff5252055ac49e4259ea9a10f79
spkelly <spkelly18@gmail.com>
2017-09-18 10:56:37 -0600

    created subNav component

---
+++ src/components/SubNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class SubNav extends Component{
+  render(){
+    return (
+      <nav className="navbar nav">
+        <a href="/" className="navbar-brand">Legitrack</a>
+      </nav>
+    )
+  }
+}
+
+export default SubNav
