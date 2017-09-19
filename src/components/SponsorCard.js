1687c961cefd1dc9a940b6cf718ea5f156654f4e
spkelly <spkelly18@gmail.com>
2017-09-18 20:18:19 -0600

    Added responsiveness to sponsor card

--- src/components/SponsorCard.js
+++ src/components/SponsorCard.js
@@ -5,9 +5,17 @@ class SponsorCard extends Component{
   render(){
     return (
       <div  className="sponsor-card" key={this.props.sponsor.people_id}>
-        <span><img  src={"https://votesmart.org/canphoto/"+this.props.sponsor.votesmart_id+"_lg.jpg"}/></span>
-        <p className="sponsor-name">{this.props.sponsor.first_name +' '+
-        this.props.sponsor.last_name}</p></div>
+        <div className="row">
+          <div className="col-md-4">
+              <span><img  src={"https://votesmart.org/canphoto/"+this.props.sponsor.votesmart_id+"_lg.jpg"}/></span>
+          </div>
+          <div className="col-md-8 sponsor-info">
+            <p className="sponsor-name">{this.props.sponsor.first_name +' '+
+            this.props.sponsor.last_name}</p>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
