863ad1e72d9164d3c0e520fea00482e1eb1eb864
spkelly <spkelly18@gmail.com>
2017-09-18 20:34:10 -0600

    fixed importstatements

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Navigation from './navbar'
-import Jumbotron from './jumbotron'
-import Promo from '../containers/promo'
-import Footer from './footer'
+import Navigation from './navbar';
+import Jumbotron from './jumbotron';
+import Promo from '../containers/promo';
+import Footer from './footer';
+
+
 class App extends Component {
   render() {
     return (
